19216f151096ef5c301f5a8a47d8bd9f1897003c
YAMANE Toshiaki <yamanetoshi@gmail.com>
2014-03-05 19:12:25 +0900

    Add candidates selection dialog

---
+++ app/src/main/java/jp/shuri/android/voicememo/ISetString.java
@@ -0,0 +1,5 @@
+package jp.shuri.android.voicememo;
+
+public interface ISetString {
+    public void setString(String str);
+}
--- app/src/main/java/jp/shuri/android/voicememo/MainActivity.java
+++ app/src/main/java/jp/shuri/android/voicememo/MainActivity.java
@@ -2,7 +2,11 @@ package jp.shuri.android.voicememo;
 
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,15 +17,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends Activity implements IMainActivity {
+public class MainActivity extends Activity implements IMainActivity, ISetString {
 
     private static final int REQUEST_CODE = 0;
+
+    private static final String KEY_CANDIDATES = "KEY_CANDIDATES";
+
+    private String mInputString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,7 @@ public class MainActivity extends Activity implements IMainActivity {
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
+            mInputString = "";
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
@@ -86,16 +98,24 @@ public class MainActivity extends Activity implements IMainActivity {
             ArrayList<String> results = data.getStringArrayListExtra(
                     RecognizerIntent.EXTRA_RESULTS);
 
-            for (int i = 0; i< results.size(); i++) {
-                // ここでは、文字列が複数あった場合に結合しています
-                resultsString += results.get(i);
-            }
+            Bundle args = new Bundle();
+            args.putStringArrayList(KEY_CANDIDATES, results);
 
-            // トーストを使って結果を表示
-            Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
+            FragmentManager manager = getFragmentManager();
+            SelectCandidatesDialogFragment dialog = new SelectCandidatesDialogFragment();
+            dialog.setArguments(args);
+            dialog.show(manager, "dialog");
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void setString(String str) {
+        mInputString += str;
+
+        // debug
+        Toast.makeText(this, mInputString, Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -123,4 +143,31 @@ public class MainActivity extends Activity implements IMainActivity {
         }
     }
 
+    public static class SelectCandidatesDialogFragment extends DialogFragment {
+        private ArrayList<String> mCandidates;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            mCandidates = getArguments().getStringArrayList(KEY_CANDIDATES);
+
+            ListView lv = new ListView(getActivity());
+            lv.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mCandidates));
+            lv.setScrollingCacheEnabled(false);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    ((ISetString)getActivity()).setString(mCandidates.get(i).toString());
+                    dismiss();
+                }
+            });
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("候補を選択");
+            builder.setNegativeButton("Cancel", null);
+            builder.setView(lv);
+            builder.setCancelable(false);
+
+            return builder.create();
+        }
+    }
 }
